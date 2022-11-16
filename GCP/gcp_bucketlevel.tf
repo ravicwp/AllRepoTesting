01fac8abe78889b08dfa51783301400a454d8776
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-11-16 13:40:44 +0530

    Update gcp_bucketlevel.tf

--- GCP/gcp_bucketlevel.tf
+++ GCP/gcp_bucketlevel.tf
@@ -2,7 +2,7 @@ provider "google"{
  region="us.west"
 }
 resource "google_storage_bucket" "Bucketlevel" {
-    name     = "TestBucket"
+    name     = "ravitestbranch"
     bucket_policy_only = true
     uniform_bucket_level_access = false
 }
