557e6a3a7da4bc43c3a3e0e99b5c84d2e31037a7
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-11-17 10:32:48 +0530

    Create bucketravi.tf

---
+++ AWS/bucketravi.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "ravi2" {
+  bucket = "examplebuckettftest"
+}
+
+resource "aws_s3_bucket" "ravitest1" {
+  bucket = "ravitestignored"
+  acl    = "private"
+  versioning {
+    enabled = false
+    mfa_delete = false
+  }
+}
+
