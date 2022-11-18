fca6677331834aff65f26028ee1fb230995de7c6
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-11-18 12:54:58 +0530

    Update ravitest.tf

--- Azure/ravitest.tf
+++ Azure/ravitest.tf
@@ -41,16 +41,6 @@ resource "azurerm_key_vault" "testing4" {
   
 }
 
-resource "azurerm_key_vault" "testing5" {
-  name                        = "des-example-keyvault_ravi6"
-  location                    = azurerm_resource_group.example.location
-  resource_group_name         = azurerm_resource_group.example.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "premium"
-  enabled_for_disk_encryption = false
-  purge_protection_enabled    = false
-  
-}
 
 resource "azurerm_key_vault" "testing6" {
   name                        = "des-example-keyvault_ravi5"
